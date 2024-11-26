8efdf0367121fbffbc58822e18b70173bed1eb02
linzizhan <linzizhan@corp.netease.com>
2024-11-26 17:11:04 +0800

    fix: logo

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'p5-react-component',
+    logo: 'https://patchwiki.biligame.com/images/persona/0/04/hfx6se1rsaf66x38fbf3ff9fv5iof4x.png',
   },
 });
