19d3de058e7a251900220d7bdac8e29d298828ea
linzizhan <linzizhan@corp.netease.com>
2024-12-13 15:02:32 +0800

    fix: github部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'p5-component'
+
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'p5-react-component',
     logo: 'https://patchwiki.biligame.com/images/persona/0/04/hfx6se1rsaf66x38fbf3ff9fv5iof4x.png',
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
